18ca484f1767ca646c10f23be1e13e586b6f412f
Timothy Harrington <tharrington999@gmail.com>
2024-12-17 17:38:02 -0600

    initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+import scrapy
+
+import numpy
+
+# need to scrape the data and store into some easily usable format
+
+# next need to gather airline incident data once I can find a source
+
+# then make the comparison between the two data sets
+
+# make some pretty graphs using numpy or something
